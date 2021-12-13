272cc37d7c709b2a97120c74e36f7364e379623d
吴浪 <773832519@qq.com>
2021-12-13 11:01:19 +0800

    提交测试

--- src/main/java/com/ls/fundstrategy/FundStrategyApplication.java
+++ src/main/java/com/ls/fundstrategy/FundStrategyApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.ls.fundstrategy.mapper")
+@MapperScan({"com.ls.fundstrategy.mapper"})
 @SpringBootApplication
 public class FundStrategyApplication {
 
